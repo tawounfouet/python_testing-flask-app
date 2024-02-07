deaff76d97412abc57be1b610768579b5ef0fce5
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-02-07 14:53:28 +0100

    test for book function

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,14 @@ from server import app
 
 @pytest.fixture
 def client():
+    """
+    Fixture to create a test client for the Flask application.
+    """
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
+
 
 @pytest.fixture
 def clubs():
@@ -39,7 +44,13 @@ def competitions():
             "name": "Fall Classic",
             "date": "2020-10-22 13:30:00",
             "numberOfPlaces": "13"
+        },
+        {
+            "name": "Winter Showdown",
+            "date": "2024-12-05 09:00:00",
+            "numberOfPlaces": "30"
         }
+
     ]
 
 @pytest.fixture
---
+++ tests/unitests/test_book.py
@@ -0,0 +1,53 @@
+import pytest
+import server
+from server import app
+
+def test_book_past_competition(client):
+    """
+    Test booking for a past competition.
+    """
+    response = client.get('/book/Fall%20Classic/Simply%20Lift')
+    assert b"Error: can not purchase a place for past competitions" in response.data
+    assert response.status_code == 200
+
+
+
+def test_book_valid_booking(client, monkeypatch, clubs, competitions):
+    """
+    Test booking for a valid competition and club.
+    """
+    
+   # Mock the get_club function
+    def patch_get_club(dummy):
+        """Mock get_club() function"""
+        return {"name": "Simply Lift", "points": 13}
+
+
+    monkeypatch.setattr(server, "get_club", patch_get_club)
+    monkeypatch.setattr(server, "competitions", competitions)
+    monkeypatch.setattr(server, "clubs", clubs)
+    
+    response = client.get('/book/Fall%20Classic/Simply%20Lift')
+    assert b"Book Places" in response.data
+    assert response.status_code == 200
+    
+
+
+
+
+def test_book_nonexistent_competition(client):
+    """
+    Test booking for a non-existent competition.
+    """
+    response = client.get('/book/Nonexistent%20Competition/Simply%20Lift')
+    assert b"Something went wrong-please try again" in response.data
+    assert response.status_code == 400
+
+
+def test_book_nonexistent_club(client):
+    """
+    Test booking for a non-existent club.
+    """
+    response = client.get('/book/Spring%20Festival/Nonexistent%20Club')
+    assert b"Something went wrong-please try again" in response.data
+    assert response.status_code == 400
